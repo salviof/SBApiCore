dcf3e23ac01dac7dec771c85203e6683420ac4e0
Salvio Furbino <salviof@gmail.com>
2024-04-28 03:07:04 -0300

    Atualizacao automática

--- src/main/java/org/coletivojava/fw/utilCoreBase/UtilSBCoreContextoSimples.java
+++ src/main/java/org/coletivojava/fw/utilCoreBase/UtilSBCoreContextoSimples.java
@@ -25,7 +25,7 @@ public class UtilSBCoreContextoSimples {
                 return contextoJunit;
             }
         }
-        contextoJunit = true;
+        contextoJunit = false;
         return false;
     }
 
